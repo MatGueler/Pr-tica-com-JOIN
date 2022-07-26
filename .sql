2c4189b120e2bd6d502c8e3db26a83603211b28a
MattGueler <mateus.gueler@gmail.com>
2022-07-26 20:29:42 -0300

    Prática

---
+++ .sql
@@ -0,0 +1,36 @@
+-- EX 1 
+
+SELECT users.id, users.name,cities.name as city FROM users JOIN cities ON users."cityId"=cities.id WHERE cities.name='Rio de Janeiro' ORDER BY users.id;
+
+
+-- EX 2
+
+SELECT testimonials.id, u1.name as writer, user2.name as recipient,
+testimonials.message
+FROM testimonials 
+JOIN users u1
+ON testimonials."writerId"=u1.id
+JOIN users user2
+ON testimonials."recipientId"=user2.id
+ORDER BY testimonials.id;
+
+
+-- EX 3
+
+SELECT educations."userId" as id,users.name, courses.name as
+course, schools.name as school,educations."endDate"
+FROM educations 
+JOIN users 
+ON educations."userId"=users.id 
+JOIN courses
+ON educations."courseId" = courses.id
+JOIN schools
+ON educations."schoolId" = schools.id
+WHERE educations."userId"=30 
+AND educations.status='finished';
+
+-- EX 4
+
+
+
+
